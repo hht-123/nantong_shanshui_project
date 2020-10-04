802fcefa00fee18ab6533fa856138a0181f3fb82
sxy8077 <70307543+sxy8077@users.noreply.github.com>
2020-10-04 16:10:50 +0800

    Add files via upload

---
+++ store/reducer.js
@@ -0,0 +1,19 @@
+const defaultState = {
+    focused: false
+};
+
+export default (state = defaultState, action) => {
+    if (action.type === 'search_focus') {
+        return {
+            focused: true
+        }
+    }
+
+    if (action.type === 'search_blur') {
+        return {
+            focused: false
+        }
+    }
+
+    return state
+}
