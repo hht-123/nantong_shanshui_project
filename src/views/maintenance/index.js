e34027c58c52ced533e38aca08d7369976f011b6
sxy8077 <807713550@qq.com>
2020-10-07 19:00:39 +0800

    sxy

--- src/views/maintenance/index.js
+++ src/views/maintenance/index.js
@@ -3,27 +3,21 @@ import { Input, Select, Button } from 'antd';
 
 import { Model } from '../../dataModule/testBone';
 
-
 import '../maintenance/style/index.less';
 import  Equipment   from '../maintenance/publicComponents/equipment.js';
 
-
-
 const model = new Model()
-
 class MaintenanceIndex extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            data:[],
-            equipment_number:'',
+            data: [],
+            equipment_number: '',
             user_company: '',
             status: '0',
-            area:''
+            area: ''
         }
-        this.changeValue = this.changeValue.bind(this);
-        
       }
     
     componentDidMount() {
@@ -48,10 +42,9 @@ class MaintenanceIndex extends Component {
               },
               this.props.whetherTest
         )
-
-        
     }
 
+    //设备状态下拉框值改变时触发的函数
     handleChange = (value) => {
       console.log(value);
       this.setState({
@@ -59,12 +52,8 @@ class MaintenanceIndex extends Component {
       })
     }
 
-  render() {
-
-    const { Option } = Select;
-
     //将数据分组
-    let getGroup = (data,key) => {
+    getGroup = (data,key) => {
       let groups = {};
       data.forEach( c => {
           let value = c[key];
@@ -73,24 +62,69 @@ class MaintenanceIndex extends Component {
       });
       return groups;
     }
-    const Newdata = getGroup(this.state.data,'area')
-    // console.log(Newdata)
   
-    
+    //搜索输入框的值改变时触发
+    changeValue = (e) => {
+      const newForm = {...this.state}
+      newForm[e.target.name] = e.target.value
+      this.setState(newForm)
+      console.log(this.state)
+    } 
+
+    //重置
+    handleClear= () => {
+      this.setState({
+       user_company: '',
+       status: '0',
+       area:''
+      })
+    }
+
+    //搜素
+    handleSearch = () => {
+      const Searchinfo = this.state
+      const me = this
+      model.fetch(
+        Searchinfo,
+        'api/MaintenanceIndex.json',
+        'get',
+        function(response) {
+          if (me.props.whetherTest === false) {
+            me.setState({
+              data: response.data,
+            })
+          } else {
+            me.setState({
+              data: response.data.data
+            })
+          }
+        },
+        function() {
+          console.log('加载失败，请重试')
+        },
+        this.props.whetherTest
+      )
+    }
+
+  render() {
+    const { Option } = Select;
+    const Newdata = this.getGroup(this.state.data,'area')
+    // console.log(Newdata)
+
     return (
         <div>
-            <div className="title" style={{overflowX: 'auto', marginTop: 60}}>
+            <div className="title" >
                 循环水智慧管家远程监控系统
             </div>
             <div className='line-top'></div>
             <div className='Search'>
                 <div className='search-area'>地区筛选:</div>
-                <Input className='area' name='area' value={ this.state.area } onChange={ this.changeValue} />
+                <Input className='area' name='area' value={ this.state.area } onChange={ this.changeValue } />
                 <div className='search-user'>用户单位:</div>
-                <Input className='user' name='user_company' value={ this.state.user_company } onChange={ this.changeValue} />
+                <Input className='user' name='user_company' value={ this.state.user_company } onChange={ this.changeValue } />
                 <div className='search-status'>设备状态:</div>
                 <div className='status'>
-                    <Select size='small' defaultValue="0" style={{ width: 120, }} onChange={this.handleChange}>
+                    <Select size='small' defaultValue="0" style={{ width: 120, }} onChange={ this.handleChange }>
                         <Option value="0">在线</Option>
                         <Option value="1">报修</Option>
                         <Option value="2">停运</Option>
@@ -98,65 +132,25 @@ class MaintenanceIndex extends Component {
                     </Select>
                 </div>
                 <div className='button1'>
-                    <Button type="primary" onClick={this.handleSearch}>搜索</Button>
+                    <Button type="primary" onClick={ this.handleSearch }>搜索</Button>
                 </div>
                 <div className='button2'>
-                    <Button onClick={this.handleClear} >重置</Button>
+                    <Button onClick={ this.handleClear } >重置</Button>
                 </div>
                 <div className='button3'>
                     <Button type="primary">新增交付属性</Button>
                 </div>
             </div >
-            {Object.keys(Newdata).map((key,index) => 
-                <div key={index} className='content' >
-                    <div className="areaName" style={{overflowX: 'auto', marginTop: 40}}>{key}</div>
+            { Object.keys(Newdata).map((key,index) => 
+                <div key={ index } className='content' >
+                    <div className="areaName" >{ key }</div>
                     <div className='line-bottom'></div>
                     { Newdata[key].map((item,index) => {
-                      return <Equipment key={index} equipment_number={item.equipment_number} user_company={item.user_company} status={item.status} />
-                    })}
+                      return <Equipment key={ index } equipment_number={ item.equipment_number } user_company={ item.user_company } status={ item.status } />
+                    }) }
                 </div>
             )}
         </div>
-    )
-  }
-
-  changeValue = (e) => {
-    const newForm = {...this.state}
-    newForm[e.target.name] = e.target.value
-    this.setState(newForm)
-    console.log(this.state)
-  }  
-
-  handleClear= () => {
-    this.setState({
-     user_company: '',
-     status: '0',
-     area:''
-    })
-  }
-
-  handleSearch = () => {
-    const Searchinfo = this.state
-    const me = this
-    model.fetch(
-      Searchinfo,
-      'api/MaintenanceIndex.json',
-      'get',
-      function(response) {
-        if (me.props.whetherTest === false) {
-          me.setState({
-            data: response.data,
-          })
-        } else {
-          me.setState({
-            data: response.data.data
-          })
-        }
-      },
-      function() {
-        console.log('加载失败，请重试')
-      },
-      this.props.whetherTest
     )
   }
 
--- src/views/maintenance/publicComponents/equipment.js
+++ src/views/maintenance/publicComponents/equipment.js
@@ -10,15 +10,13 @@ class Equipment extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        
     }
-    this.enterEquipment = this.enterEquipment.bind(this);
   }
 
   componentDidMount() {
   }
 
-  handleStatus(numb) {
+  handleStatus = (numb) => {
     if ( numb === '0' ) {
       return  '在线'
     }else if (numb === '1') {
@@ -30,9 +28,9 @@ class Equipment extends Component {
     }
   }
 
-  handleStatusColor(numb) {
+  handleStatusColor = (numb) => {
     if ( numb === '0' ) {
-      return  { background:'greenyellow'}
+      return  { background:'green'}
     }else if (numb === '1') {
       return  { background:'red'}
     }else if (numb === '2') {
@@ -50,10 +48,10 @@ class Equipment extends Component {
     
     return (
                 <span className='equipment'>
-                    <img className='Pic-equipment' alt='shebei' src={Shebei} onClick={this.enterEquipment} />
-                    <div className='equipmentID'>{this.props.equipment_number}</div>
-                    <div className='company'>{this.props.user_company}</div>
-                    <div className='status' style={this.handleStatusColor(this.props.status)} > {this.handleStatus(this.props.status)}</div>
+                    <img className='Pic-equipment' alt='shebei' src={ Shebei } onClick={ this.enterEquipment } />
+                    <div className='equipmentID'>{ this.props.equipment_number }</div>
+                    <div className='company'>{ this.props.user_company }</div>
+                    <div className='status' style={ this.handleStatusColor(this.props.status) } > { this.handleStatus(this.props.status) }</div>
                 </span>
     )
   }
